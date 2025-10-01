ac1dfec9249824d289219441eb37857dff8af64c
voiceinthedark <darkrisingforce@gmail.com>
2025-10-01 20:03:06 +0300

    Newsletter and footer
    feat: add newsletter and footer elements

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,9 +3,18 @@ import './footer.scss'
 const Footer = () => {
   return (
     <section className="footer">
-      <div>left</div>
-      <div>middle</div>
-      <div>right</div>
+      <div className='contact'>
+        <h2>Contact Us</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+      </div>
+      <div className='phone'>
+        <h2>Phone</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam saepe, in expedita fugiat earum incidunt odit, soluta corrupti quo, quas placeat repellendus magnam sit sapiente. Ab distinctio ad ullam quaerat.</p>
+      </div>
+      <div className='location'>
+        <h2>Location</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum assumenda commodi impedit error maiores saepe, recusandae temporibus, explicabo sint id sed ratione repellendus nisi dolores labore pariatur. Quaerat, sint repellat.</p>
+      </div>
     </section>
   )
 }
--- src/components/layout/Newsletter.jsx
+++ src/components/layout/Newsletter.jsx
@@ -1,15 +1,26 @@
+import { Link } from 'react-router'
 import './newsletter.scss'
 
 const NewsLetter = () => {
   return (
     <section className="newsletter">
-      <div>
-        left
+      <div className='newsletter-menu'>
+        <h2>Menu</h2>
+        <ul>
+          <li><Link to={'shop'}>Shop</Link></li>
+          <li><Link to={'categories'}>Category</Link></li>
+          <li><Link>About</Link></li>
+        </ul>
       </div>
-      <div>
-        middle
+      <div className='newsletter-sub'>
+        <h2>NewsLetter</h2>
+        <button>Subscribe</button>
       </div>
-      <div>right</div>
+      <div className='newsletter-avant'>
+        <h2>AvantGardia</h2>
+        <p>One hall mall</p>
+
+      </div>
     </section>
   )
 }
